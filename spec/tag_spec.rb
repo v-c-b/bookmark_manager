35bc854e705fb576797df52e6517f17749ee359b
Volker Bergner <volker@bergner.one>
2019-09-13 12:33:37 +0100

    Test for tag creation fixed

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -4,9 +4,8 @@ describe Tag do
     it 'lets user add a tag to a bookmark' do
       trunc_test_database
       PG.connect dbname: 'bookmark_manager_test'
-      bookmark = Bookmark.add(title: 'BBC', url: 'www.bbc.co.uk')
-      test_tag = Tag.create(bookmark.id, 'test tag')
-      expect(Tag.all[0].content).to eq()'test tag')
+      test_tag = Tag.create('test tag')
+      expect(Tag.all[0].content).to eq('test tag')
     end
   end
 end
--- spec/tags_feature_spec.rb
+++ spec/tags_feature_spec.rb
@@ -2,10 +2,12 @@ feature 'Tags' do
   scenario 'user can add a tag' do
     trunc_test_database
     populate_test_database
-    visit '/bookmarks'
-    first('.bookmark').click_button 'Add tag'
+    visit '/'
+    click_button 'Add tag'
     fill_in :tag, with: 'xyz*1453'
     click_button 'Submit'
+    visit '/'
+    click_button 'Add tag'
     expect(page).to have_content('xyz*1453')
   end
 end
